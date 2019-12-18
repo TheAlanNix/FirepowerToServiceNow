d1303b126f7dbed6e3e599408f0537b55cfc54ba
Alan Nix <alanneilnix@gmail.com>
2019-12-17 22:36:11 -0500

    Code cleanup to implement a standard EventQueue class.

--- listener/event_listener.py
+++ listener/event_listener.py
@@ -9,6 +9,7 @@ import json
 import os
 import re
 import socketserver
+import time
 
 import pika
 
@@ -17,18 +18,80 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+event_queue = None
+
+
+class EventQueue():
+    """
+    A class to handle event queuing.
+    """
+
+    def __init__(self, host, queue_name="events", debug=False):
+        """
+        Initialize the EventQueue object.
+        """
+
+        self._debug = debug
+        self._rabbit_mq_channel = None
+        self._rabbit_mq_connection = None
+        self._rabbit_mq_host = host
+        self._rabbit_mq_queue = None
+        self._rabbit_mq_queue_name = queue_name
+
+    def close(self):
+        """
+        Close the RabbitMQ connection.
+        """
+
+        # Close the connection
+        self._rabbit_mq_connection.close()
+
+    def create_channel(self):
+        """
+        Create a RabbitMQ channel.
+        """
+
+        # Create RabbitMQ connection parameters
+        connection_parameters = pika.ConnectionParameters(heartbeat=600, host=self._rabbit_mq_host)
+
+        # Build the RabbitMQ connection
+        self._rabbit_mq_connection = pika.BlockingConnection(connection_parameters)
+
+        # Get a channel for the RabbitMQ connection
+        self._rabbit_mq_channel = self._rabbit_mq_connection.channel()
+
+    def create_queue(self):
+        """
+        Create a RabbitMQ queue.
+        """
+
+        # If we have a channel, create a queue
+        if self._rabbit_mq_channel:
+
+            # Declare a new queue for events
+            self._rabbit_mq_queue = self._rabbit_mq_channel.queue_declare(queue=self._rabbit_mq_queue_name)
+
+    def queue_data(self, data):
+        """
+        Put the provided data onto a RabbitMQ queue.
+        """
+
+        # Publish the event to RabbitMQ
+        self._rabbit_mq_channel.basic_publish(exchange="", routing_key=self._rabbit_mq_queue_name, body=json.dumps(data))
+
+        print(" [x] Event published to queue.")
+
+        # If debugging enabled, print details
+        if self._debug:
+            print(f"Event details:\n{json.dumps(data, indent=4)}")
+
 
 class FirepowerSyslogHandler():
     """
     A class to parse Firepower syslog events.
     """
 
-    __rabbit_mq_host = None
-
-    def __init__(self):
-        self.__rabbit_mq_host = os.getenv("RABBIT_MQ_HOST")
-
-    def _parse_event(self, data):
+    def parse_event(self, data):
         """
         Parse the data using regex to extract the pertinent Firepower data.
         """
@@ -67,35 +130,11 @@ class FirepowerSyslogHandler():
                 "dst_geo": parsed_event.group(16),
             }
 
+            # Return the parsed event
             return event_json
 
         else:
             return None
-
-    def _queue_event(self, event_json):
-        """
-        Place the parsed event on a RabbitMQ queue.
-        """
-
-        # Create RabbitMQ connection parameters
-        connection_parameters = pika.ConnectionParameters(host=self.__rabbit_mq_host)
-
-        # Build the RabbitMQ connection
-        connection = pika.BlockingConnection(connection_parameters)
-
-        # Get a channel for the RabbitMQ connection
-        channel = connection.channel()
-
-        # Declare a new queue for events
-        channel.queue_declare(queue="events")
-
-        # Publish the event to RabbitMQ
-        channel.basic_publish(exchange="", routing_key="events", body=json.dumps(event_json))
-
-        print(f"Event publiished to queue.  Event details:\n{json.dumps(event_json, indent=4)}")
-
-        # Close the connection
-        connection.close()
 
 
 class SyslogHandler(socketserver.BaseRequestHandler):
@@ -116,25 +155,51 @@ class SyslogHandler(socketserver.BaseRequestHandler):
         event_parser = FirepowerSyslogHandler()
 
         # Try to parse the event data
-        event_json = event_parser._parse_event(self.data)
+        event_json = event_parser.parse_event(self.data)
 
-        # If the event was parsed
+        # If we were able to parse the event, then put it on the queue
         if event_json:
-
-            # Store the event
-            event_parser._queue_event(event_json)
+            event_queue.queue_data(event_json)
 
 
 if __name__ == "__main__":
 
+    # Instantiate an EventQueue object
+    event_queue = EventQueue(os.getenv("RABBIT_MQ_HOST"), "events")
+
+    # Wait for RabbitMQ to become available
+    while True:
+        try:
+            # Create a RabbitMQ channel if not already done
+            if not event_queue._rabbit_mq_channel:
+                print(" [x] Creating RabbitMQ channel...")
+                event_queue.create_channel()
+                print(" [x] RabbitMQ channel created.")
+
+            # Create a RabbitMQ queue if not already done
+            if not event_queue._rabbit_mq_queue:
+                print(" [x] Creating RabbitMQ queue...")
+                event_queue.create_queue()
+                print(" [x] RabbitMQ queue created.")
+
+            break
+        except:
+            print(" [x] Unable to create RabbitMQ channel. Retrying...")
+            # If we weren't able to create the channel, wait X seconds and try again
+            time.sleep(5)
+
+    # Now that an event queue is ready, open up a syslog listener for events
+    # Parse the syslog port from environment variables
+    syslog_port = int(os.getenv("SYSLOG_PORT"))
+
     try:
-
-        syslog_port = int(os.getenv("SYSLOG_PORT"))
-
+        # Open a UDP listener on the specified port, and listen forever
         server = socketserver.UDPServer(("0.0.0.0", syslog_port), SyslogHandler)
-        print(f"Server listening on port {syslog_port}...")
+        print(f" [x] Server listening on port {syslog_port}...")
         server.serve_forever()
     except (IOError, SystemExit):
+        event_queue.close()
         raise
     except KeyboardInterrupt:
+        event_queue.close()
         print("\nCrtl+C Pressed. Shutting down.")
--- publisher/event_publisher.py
+++ publisher/event_publisher.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This module is used to import Firepower syslog events into ServiceNow
+This module is used to consume RabbitMQ events, then publish them to ServiceNow.
 """
 
 import json
@@ -18,44 +18,85 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class ServiceNowPublisher():
+class EventQueue():
     """
-    A class to parse Firepower syslog events.
+    A class to handle event queuing.
     """
 
-    __debug = None
-    __rabbit_mq_host = None
-    __snow_tenant = None
-    __snow_username = None
-    __snow_password = None
-
-    def __init__(self, debug=False):
-        self.__debug = debug
-        self.__rabbit_mq_host = os.getenv("RABBIT_MQ_HOST")
-        self.__snow_tenant = os.getenv("SNOW_TENANT")
-        self.__snow_username = os.getenv("SNOW_USERNAME")
-        self.__snow_password = os.getenv("SNOW_PASSWORD")
-
-    def create_queue_channel(self):
+    def __init__(self, host, queue_name="events", debug=False):
         """
-        Create a connection to RabbitMQ and return the channel.
+        Initialize the EventQueue object.
+        """
+
+        self._debug = debug
+        self._rabbit_mq_channel = None
+        self._rabbit_mq_connection = None
+        self._rabbit_mq_host = host
+        self._rabbit_mq_queue = None
+        self._rabbit_mq_queue_name = queue_name
+
+    def close(self):
+        """
+        Close the RabbitMQ connection.
+        """
+
+        # Close the connection
+        self._rabbit_mq_connection.close()
+
+    def create_channel(self):
+        """
+        Create a RabbitMQ channel.
         """
 
         # Create RabbitMQ connection parameters
-        connection_parameters = pika.ConnectionParameters(host=self.__rabbit_mq_host)
+        connection_parameters = pika.ConnectionParameters(heartbeat=600, host=self._rabbit_mq_host)
 
         # Build the RabbitMQ connection
-        connection = pika.BlockingConnection(connection_parameters)
-
-        print(f" [x] Connected to RabbitMQ.")
+        self._rabbit_mq_connection = pika.BlockingConnection(connection_parameters)
 
         # Get a channel for the RabbitMQ connection
-        channel = connection.channel()
+        self._rabbit_mq_channel = self._rabbit_mq_connection.channel()
 
-        print(f" [x] Built RabbitMQ channel.")
+    def create_queue(self):
+        """
+        Create a RabbitMQ queue.
+        """
 
-        return channel
+        # If we have a channel, create a queue
+        if self._rabbit_mq_channel:
 
+            # Declare a new queue for events
+            self._rabbit_mq_queue = self._rabbit_mq_channel.queue_declare(queue=self._rabbit_mq_queue_name)
+
+    def queue_data(self, data):
+        """
+        Put the provided data onto a RabbitMQ queue.
+        """
+
+        # Publish the event to RabbitMQ
+        self._rabbit_mq_channel.basic_publish(exchange="", routing_key=self._rabbit_mq_queue_name, body=json.dumps(data))
+
+        print(" [x] Event published to queue.")
+
+        # If debugging enabled, print details
+        if self._debug:
+            print(f"Event details:\n{json.dumps(data, indent=4)}")
+
+
+class ServiceNowPublisher():
+    """
+    A class to publish events to ServiceNow.
+    """
+
+    def __init__(self, debug=False):
+        """
+        Initialize the ServiceNowPublisher object.
+        """
+
+        self._debug = debug
+        self._snow_tenant = os.getenv("SNOW_TENANT")
+        self._snow_username = os.getenv("SNOW_USERNAME")
+        self._snow_password = os.getenv("SNOW_PASSWORD")
 
     def submit_to_snow(self, ch, method, properties, body):
         """
@@ -66,7 +107,7 @@ class ServiceNowPublisher():
         body = json.loads(body)
 
         # Build the ServiceNow API URL
-        snow_url = "https://{}.service-now.com/api/now/table/incident".format(self.__snow_tenant)
+        snow_url = f"https://{self._snow_tenant}.service-now.com/api/now/table/incident"
 
         # Specify headers for the ServiceNow POST request
         headers = {"Content-Type": "application/json", "Accept": "application/json"}
@@ -80,33 +121,46 @@ class ServiceNowPublisher():
         }
 
         # Send the data to ServiceNow
-        response = requests.post(snow_url, auth=(self.__snow_username, self.__snow_password), headers=headers, data=json.dumps(data))
+        response = requests.post(snow_url, auth=(self._snow_username, self._snow_password), headers=headers, data=json.dumps(data))
 
-        # Print logging
-        print(f"Status: {response.status_code}\nHeaders: {response.headers}\nResponse: {response.json()}")
+        print(f" [x]Event published to ServiceNow. HTTP response code: {response.status_code}")
+
+        # If debugging enabled, print details
+        if self._debug:
+            print(f"Headers: {response.headers}\nResponse: {response.text}")
 
 
 if __name__ == "__main__":
 
-    # Create the ServiceNowPublisher object
-    servicenow_publisher = ServiceNowPublisher()
+    # Instantiate an EventQueue object
+    event_queue = EventQueue(os.getenv("RABBIT_MQ_HOST"), "events")
 
     # Wait for RabbitMQ to become available
     while True:
         try:
-            # Create a channel on RabbitMQ
-            channel = servicenow_publisher.create_queue_channel()
+            # Create a RabbitMQ channel if not already done
+            if not event_queue._rabbit_mq_channel:
+                print(" [x] Creating RabbitMQ channel...")
+                event_queue.create_channel()
+                print(" [x] RabbitMQ channel created.")
+
+            # Create a RabbitMQ queue if not already done
+            if not event_queue._rabbit_mq_queue:
+                print(" [x] Creating RabbitMQ queue...")
+                event_queue.create_queue()
+                print(" [x] RabbitMQ queue created.")
+
             break
         except:
-            print(f" [x] Unable to create RabbitMQ channel. Retrying...")
+            print(" [x] Unable to create RabbitMQ channel. Retrying...")
             # If we weren't able to create the channel, wait X seconds and try again
             time.sleep(5)
 
-    # Declare a new queue for events
-    channel.queue_declare(queue="events")
+    # Instantiate the ServiceNowPublisher object
+    servicenow_publisher = ServiceNowPublisher()
 
-    # Set up an event consumer on the provided channel
-    channel.basic_consume(queue="events", on_message_callback=servicenow_publisher.submit_to_snow, auto_ack=True)
-
-    # Start consuming queued events
-    channel.start_consuming()
+    # Set up an event consumer on the provided channel, and start consuming
+    event_queue._rabbit_mq_channel.basic_consume(queue="events",
+                                                 on_message_callback=servicenow_publisher.submit_to_snow,
+                                                 auto_ack=True)
+    event_queue._rabbit_mq_channel.start_consuming()
